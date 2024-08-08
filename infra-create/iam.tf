4fd7f382f14fd58bbe4e6909da2b3ad141059924
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-08-08 16:15:48 +0530

    jenkins

--- infra-create/iam.tf
+++ infra-create/iam.tf
@@ -1,19 +1,20 @@
 resource "aws_iam_role" "terraformrole" {
   name = "${var.name}-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-  assume_role_policy = jsondecode({
-    "Version": "2012-10-17"
-    "Statement": [
-        {
-           "Action": "sts:AssumeRole"
-            "Effect": "Allow"
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            }
-        },
-      ]
-    })
 
   tags = {
     tag-key = "${var.name}-role"
